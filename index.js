a198ab37062fbca3d59654abff702df4dc1c7428
neville777 <neville.james@student.moringaschool.com>
2023-09-14 02:16:30 +0300

    arithmetic assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+// Add
+function add(a, b) {
+    return a + b;
+}
+// Subtract
+function subtract(a, b) {
+    return a - b;
+}
+// Multiply
+function multiply(a, b) {
+    return a * b;
+}
+// Divide
+function divide(a, b) {
+    return a / b;
+}
+// Increment
+function increment(n) {
+    return n += 1;
+}
+// Decrement
+function decrement(n) {
+    return n -= 1;
+}
+// parseInt an integer
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+// parseFloat 
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
